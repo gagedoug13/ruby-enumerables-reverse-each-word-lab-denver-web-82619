697b4e4ac97348e050a02981f9e125baf9419326
Gage Thornberry <gagedoug13@gmail.com>
2019-08-20 00:58:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(string)
-  new_array = string.split(" ")
+def reverse_each_word(sentence)
+  new_array = sentence.split(" ")
   final_array = []
   new_array.collect do |string|
-    final_array.push(string.reverse)
+    final_array.push(sentence.reverse)
   end
   p final_array.join(" ")
 end
