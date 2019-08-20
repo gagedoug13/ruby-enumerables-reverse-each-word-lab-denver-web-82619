9b384ae5658a48df7e21ac70356813a1e1119fec
Gage Thornberry <gagedoug13@gmail.com>
2019-08-20 00:58:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   new_array = sentence.split(" ")
   final_array = []
   new_array.collect do |string|
-    final_array.push(sentence.reverse)
+    final_array.push(string.reverse)
   end
   p final_array.join(" ")
 end
